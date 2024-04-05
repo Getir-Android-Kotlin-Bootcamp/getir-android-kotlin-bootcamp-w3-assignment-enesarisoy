4c0db8e43ba6a23149012d3adfc625c04ad8ad86
Enes <arisoyeness@gmail.com>
2024-04-05 11:48:48 +0300

    Extra. Look description.
    Description: I chaged the AccountFragment UI. So you can see the UI instead blank screen. After you navigate to the nav_account, you can see the changes easily.

--- app/src/main/java/com/getir/patika/foodcouriers/presentation/account/AccountFragment.kt
+++ app/src/main/java/com/getir/patika/foodcouriers/presentation/account/AccountFragment.kt
@@ -4,17 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.getir.patika.foodcouriers.PagerAdapter
 import com.getir.patika.foodcouriers.R
+import com.getir.patika.foodcouriers.databinding.FragmentAccountBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.wada811.viewbindingktx.viewBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 /**
  * A simple [Fragment] subclass.
  * Use the [AccountFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AccountFragment : Fragment() {
+class AccountFragment : Fragment(R.layout.fragment_account) {
+
+    private val binding by viewBinding(FragmentAccountBinding::bind)
+    private lateinit var pagerAdapter: PagerAdapter
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -27,12 +39,25 @@ class AccountFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Toast.makeText(requireContext(), "Account Fragment", Toast.LENGTH_SHORT).show()
+
+        binding.apply {
+            pagerAdapter = PagerAdapter(childFragmentManager, lifecycle).apply {
+                addFragment(CreateAccountFragment())
+                addFragment(LoginAccountFragment())
+            }
+            viewPagerAccount.adapter = pagerAdapter
+
+            TabLayoutMediator(tabAccount, viewPagerAccount) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "Create Account"
+                    1 -> tab.text = "Login"
+                }
+            }.attach()
+        }
     }
 
     companion object {
